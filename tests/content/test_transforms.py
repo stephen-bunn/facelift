3a6109286f5dbb8fd505d0283887c7b85a8db402
Stephen Bunn <stephen@bunn.io>
2020-09-18 17:31:59 -0400

    Adding docstring for transform tests
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- tests/content/test_transforms.py
+++ tests/content/test_transforms.py
@@ -2,8 +2,7 @@
 # Copyright (c) 2020 Stephen Bunn <stephen@bunn.io>
 # ISC License <https://choosealicense.com/licenses/isc>
 
-"""
-"""
+"""Contains tests for content transform functions."""
 
 from unittest.mock import patch
 

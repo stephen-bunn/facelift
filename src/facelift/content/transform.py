0048dbf2d9ae6341fca866fa38217cb49361ae19
Stephen Bunn <stephen@bunn.io>
2020-09-28 18:44:31 -0400

    Updating content.transform docstrings
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- src/facelift/content/transform.py
+++ src/facelift/content/transform.py
@@ -2,7 +2,13 @@
 # Copyright (c) 2020 Stephen Bunn <stephen@bunn.io>
 # ISC License <https://choosealicense.com/licenses/isc>
 
-"""Contains some common necessary frame transformation helper methods."""
+"""Contains some common necessary frame transformation helper methods.
+
+Attributes:
+    DEFAULT_INTERPOLATION (int):
+        The default type of interpolation to use in transforms that require an
+        interpolation method. Defaults to ``cv2.INTER_AREA``.
+"""
 
 import warnings
 from typing import Optional, Tuple
@@ -18,11 +24,17 @@ DEFAULT_INTERPOLATION: int = cv2.INTER_AREA
 def copy(frame: Frame) -> Frame:
     """Copy the given frame to a new location in memory.
 
+    Examples:
+        >>> from facelift.content.transform import copy
+        >>> copied_frame = copy(frame)
+        >>> assert frame == copied_frame
+        >>> assert frame is not copied_frame
+
     Args:
-        frame (Frame): The frame to copy
+        frame (:attr:`~.types.Frame`): The frame to copy
 
     Returns:
-        Frame: An exact copy of the given frame
+        :attr:`~.types.Frame`: An exact copy of the given frame
     """
 
     return frame.copy()
@@ -33,38 +45,52 @@ def scale(
 ) -> Frame:
     """Scale a given frame down or up depending on the given scale factor.
 
-    Downscaling a frame can be performed with a ``scale`` factor >0 and <1.
-    For example, scaling a frame to half of its original size would require a scale
-    factor of 0.5.
-    Upscaling a frame with this method is **very** naive and unoptimal.
-    However, any value >1 will result in a upscaled frame.
-    For example, scaling a frame to double its original size would require a scale
-    factor of 2.
-    Following this logic, a scale factor of 1 would result in absolutely no change to
-    the given frame.
+    Examples:
+        Downscaling a frame can be performed with a ``scale`` factor >0 and <1.
+        For example, scaling a frame to half of its original size would require a scale
+        factor of 0.5.
 
-    .. important::
-        This transformation will return the exact same ``frame`` instance as the one
-        provided in the following cases:
+        >>> from facelift.content.transform import scale
+        >>> assert frame.shape[:1] == [512, 512]
+        >>> downscaled_frame = scale(frame, 0.5)
+        >>> assert downscaled_frame.shape[:1] == [256, 256]
+
+        Upscaling a frame with this method is **very** naive and unoptimal.
+        However, any value >1 will result in a upscaled frame.
+        For example, scaling a frame to double its original size would require a scale
+        factor of 2.
+
+        >>> from facelift.content.transform import scale
+        >>> assert frame.shape[:1] == [512, 512]
+        >>> upscaled_frame = scale(frame, 2)
+        >>> assert upscaled_frame.shape[:1] == [1024, 1024]
+
+        Following this logic, a scale factor of 1 would result in absolutely no change
+        to the given frame.
+
+    .. warning::
+        This transformation will return the **exact same frame instance** as the one
+        provided through the ``frame`` parameter in the following cases:
 
             1. If a factor of exactly ``1`` is given.
-                In this case the scale operation would result in no change.
+               In this case the scale operation would result in no change.
 
             2. The given frame has factor less than ``1`` a width or height of 1px.
-                In this case we are attempting to scale down the given frame and we
-                cannot scale down the frame any further without producing a 0px frame.
+               In this case we are attempting to scale down the given frame and we
+               cannot scale down the frame any further without producing a 0px frame.
 
     Args:
-        frame (Frame): The frame to scale
+        frame (:attr:`~.types.Frame`): The frame to scale
         factor (float): The factor to scale the given frame
-        interpolation (Optional[int], optional): The type of interpolation to use in the
-            scale operation. Defaults to DEFAULT_INTERPOLATION.
+        interpolation (Optional[int], optional):
+            The type of interpolation to use in the scale operation.
+            Defaults to :attr:`~DEFAULT_INTERPOLATION`.
 
     Raises:
         ValueError: When the given scale factor is not positive
 
     Returns:
-        Frame: The newly scaled frame
+        :attr:`~.types.Frame`: The newly scaled frame
     """
 
     if factor <= 0:
@@ -106,15 +132,44 @@ def resize(
         * If ``lock_aspect`` is falsy, the resize operation will only scale the provided
           dimension while keeping the original size of the unprovided dimension.
 
+    Examples:
+        Resize a frame's width while keeping the height relative:
+
+        >>> from facelift.content.transform import resize
+        >>> assert frame.shape[:1] == [512, 512]
+        >>> resized_frame = resize(frame, width=256, lock_aspect=True)
+        >>> assert resized_frame.shape[:1] == [256, 256]
+
+        Resize a frame's width while keeping the original height:
+
+        >>> from facelift.content.transform import resize
+        >>> assert frame.shape[:1] == [512, 512]
+        >>> resized_frame = resize(frame, width=256, lock_aspect=False)
+        >>> assert resized_frame.shape[:1] == [512, 256]
+
+        Resize both a frame's width and height:
+
+        >>> from facelift.content.transform import resize
+        >>> assert frame.shape[:1] == [512, 512]
+        >>> resized_frame = resize(frame, width=256, height=128)
+        >>> assert resized_frame.shape[:1] == [128, 256]
+
     Args:
-        frame (Frame): The frame to resize
-        width (Optional[int], optional): The exact width to resize the frame to.
-        height (Optional[int], optional): The exact height to resize the frame to.
-        interpolation (int, optional): The type of interpolation to use in the resize
-            operation. Defaults to DEFAULT_INTERPOLATION
+        frame (:attr:`~.types.Frame`):
+            The frame to resize
+        width (Optional[int], optional):
+            The exact width to resize the frame to.
+        height (Optional[int], optional):
+            The exact height to resize the frame to.
+        lock_aspect (bool, optional):
+            Whether to keep the width and height relative when only given one value.
+            Defaults to True.
+        interpolation (int, optional):
+            The type of interpolation to use in the resize operation.
+            Defaults to :attr:`~DEFAULT_INTERPOLATION`.
 
     Returns:
-        Frame: The newly resized frame
+        :attr:`~.types.Frame`: The newly resized frame
     """
 
     if width == 0 or height == 0:
@@ -169,14 +224,17 @@ def rotate(
 ) -> Frame:
     """Rotate a frame while keeping the whole frame visible.
 
-    .. important::
+    Examples:
+        >>> from facelift.content.transform import rotate
+        >>> rotated_90 = rotate(frame, 90)
+        >>> rotated_neg_90 = rotate(frame, -90)
+
+    .. warning::
         This transform typically will produce larger frames since we are producing a
         rotated frame while keeping the original frame completely visible.
         This means if we do a perfect 45 degree rotation on a 512x512 frame we will
         produce a 724x724 frame since the 512x512 frame is now on a angle that requires
         a larger container.
-        **This really only the case for frames equal to or larger than 90px in either
-        or width.**
 
         Be cautious when using rotation.
         Most of the time you do not need to rotate on any angles other than 90, 180, and
@@ -184,13 +242,16 @@ def rotate(
         However, this isn't *always* true.
 
     Args:
-        frame (Frame): The frame to rotate
-        degrees (int): The number of degress to rotate the given frame
-        interpolation (int, optional): The type of interpolation to use in the produced
-            rotation matrix. Defaults to DEFAULT_INTERPOLATION.
+        frame (:attr:`~.types.Frame`):
+            The frame to rotate
+        degrees (int):
+            The number of degress to rotate the given frame
+        interpolation (int, optional):
+            The type of interpolation to use in the produced rotation matrix.
+            Defaults to :attr:`~DEFAULT_INTERPOLATION`.
 
     Returns:
-        Frame: The newly rotated frame
+        :attr:`~.types.Frame`: The newly rotated frame
     """
 
     if abs(degrees) in (0, 360):
@@ -223,17 +284,28 @@ def rotate(
 def crop(frame: Frame, start: Tuple[int, int], end: Tuple[int, int]) -> Frame:
     """Crop the given frame between two top-left to bottom-right points.
 
+    Examples:
+        Crop a frame from the first pixel to the center pixel.
+
+        >>> from facelift.content.transform import crop
+        >>> assert frame.shape[:1] == [512, 512]
+        >>> cropped_frame = crop(frame, (0, 0), (256, 256))
+        >>> assert cropped_frame.shape[:1] == [256, 256]
+
     Args:
-        frame (Frame): The frame to crop
-        start (Tuple[int, int]): The top-left point to start the crop at
-        end (Tuple[int, int]): The bottom-right point to end the crop at
+        frame (:attr:`~.types.Frame`):
+            The frame to crop
+        start (Tuple[int, int]):
+            The top-left point to start the crop at
+        end (Tuple[int, int]):
+            The bottom-right point to end the crop at
 
     Raises:
-        ValueError: When the given starting crop point appears after the given ending
-            crop point
+        ValueError:
+            When the given starting crop point appears after the given ending crop point
 
     Returns:
-        Frame: The newly cropped frame
+        :attr:`~.types.Frame`: The newly cropped frame
     """
 
     left, top = start
@@ -258,21 +330,28 @@ def translate(
 ) -> Frame:
     """Translate the given frame a specific distance away from its origin.
 
-    This translation maintains the original size of the given frame.
-    So a 512x512 frame translated 90px on the x-axis will still be 512x512 and space
-    where the frame use to take up will be essentially nulled out.
+    Examples:
+        >>> from facelift.content.transform import translate
+        >>> translated_neg_90_x_frame = translate(frame, delta_x=-90)
+
+    .. important::
+        This translation retains the original size of the given frame.
+        So a 512x512 frame translated 90px on the x-axis will still be 512x512 and space
+        where the frame use to take up will be essentially nulled out.
 
     Args:
-        frame (Frame): The frame to translate
-        delta_x (Optional[int], optional): The pixel distance to translate the frame on
-            the x-axis.
-        delta_y (Optional[int], optional): The pixel distance to translate the frame on
-            the y-axis.
-        interpolation (int, optional): The type of interpolation to use during the
-            translation. Defaults to DEFAULT_INTERPOLATION.
+        frame (:attr:`~.types.Frame`):
+            The frame to translate
+        delta_x (Optional[int], optional):
+            The pixel distance to translate the frame on the x-axis.
+        delta_y (Optional[int], optional):
+            The pixel distance to translate the frame on the y-axis.
+        interpolation (int, optional):
+            The type of interpolation to use during the translation.
+            Defaults to :attr:`~DEFAULT_INTERPOLATION`.
 
     Returns:
-        Frame: The newly translated frame
+        :attr:`~.types.Frame`: The newly translated frame
     """
 
     if delta_x is None and delta_y is None:
@@ -292,11 +371,18 @@ def translate(
 def grayscale(frame: Frame) -> Frame:
     """Convert the given frame to grayscale.
 
+    This helper is useful *sometimes* for classification as color doesn't matter as much
+    during face encoding.
+
+    Examples:
+        >>> from facelift.content.transform import grayscale
+        >>> grayscale_frame = grayscale(bgr_frame)
+
     Args:
-        frame (Frame): The BGR frame to convert to grayscale
+        frame (:attr:`~.types.Frame`): The BGR frame to convert to grayscale
 
     Returns:
-        Frame: The newly grayscaled frame
+        :attr:`~.types.Frame`: The newly grayscaled frame
     """
 
     return cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY)
@@ -305,11 +391,19 @@ def grayscale(frame: Frame) -> Frame:
 def rgb(frame: Frame) -> Frame:
     """Convert the given frame to RGB.
 
+    This helper transform is typically needed when working with other image processing
+    libraries such as `pillow <https://pillow.readthedocs.io/en/stable/>`_ as they work
+    in RGB coordinates while OpenCV works in BGR coordinates.
+
+    Examples:
+        >>> from facelift.content.transform import rgb
+        >>> rgb_frame = rgb(bgr_frame)
+
     Args:
-        frame (Frame): The BGR frame to conver to RGB
+        frame (:attr:`~.types.Frame`): The BGR frame to conver to RGB
 
     Returns:
-        Frame: The new RGB frame
+        :attr:`~.types.Frame`: The new RGB frame
     """
 
     return cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2RGB)
